6e34671682c3d73bf0d69f4f1d343990fa2868f4
yuhang lian <yuhanglian@yuhangs-MacBook.local>
2018-02-21 19:43:01 -0800

    react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import images from './stuffed_mushroom_pic.jpg';
+import recipe from '/recipe'
 
 class App extends Component {
+      constructor(props){
+        super(props);
+        this.state = {
+          dataList:[{
+            id: 1,
+            pic: "stuffed_mushroom_pic.jpg",
+            text: "Nice Mushroom"
+          }]
+        }
+      }
+      populate(){
+        console.log("Loaded Page");
+
+        var list = document.getElementsByClassName("item");
+        console.log(list);
+        for(var i = 0; i < this.state.dataList.length;i++){
+          list[i].innerHTML ="<h1>ok</h1>" //"<img src="+this.state.dataList[i].pic+" height='200' width='400' alt= 'Unable to load Image'/><br /><a href='detail_page.html'>"+this.state.dataList[i].text+"</a>"+"<br /><button onclick='deleteItem("+ this.state.dataList[i].id +")'>Delete</button>";
+        }
+      }
+    
+
   render() {
+    console.log(recipe);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header>
+        <div id="brand">CookBook.co</div>
+      </header>
+      <div id="container">
+          <div className="item"><img src={images} height='200' width='400' alt="unable to load"></img><br /><a href='detail_page.html'>{this.state.dataList[0].text}</a></div>
+          <div className="item"></div>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className='item'></div>
+          <div className='item'></div>
+      </div>
       </div>
     );
   }
+
+
 }
 
 export default App;
+
