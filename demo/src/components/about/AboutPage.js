c6da80eedb493d67e085fc3a4710d18e677b83d2
Qinyun <tarringcai@gmail.com>
2018-03-02 15:04:30 -0800

    about page

--- demo/src/components/about/AboutPage.js
+++ demo/src/components/about/AboutPage.js
@@ -4,8 +4,11 @@ class AboutPage extends React.Component {
   render() {
     return (
       <div>
-        <h1>About</h1>
+        <h1>About Us</h1>
+        <br/>
         <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
+        <br/>
+        <p>It will show you the full recipe and ways to cook in detail. You can simply cook at Home!</p>
       </div>
     );
   }
